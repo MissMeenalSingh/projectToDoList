3b020bbe5894caae2037e69cac28f4fe01bf48a4
MissMeenalSingh <meenalsingh223@gmail.com>
2023-05-02 22:06:19 +0530

    schema without dynamic

---
+++ Project-todolist-jquery/code/script.js
@@ -0,0 +1,45 @@
+// $(()=>{
+
+// })
+
+let inputNewTask = $('#inpNewTask')
+let btnAddTask = $('#btnAddTask')
+let btnClearTask = $('#btnClearTask')
+let ulTasks = $('#ulTasks')
+let count = 0
+
+
+function addItem() {
+
+    let className = 'list-group-item-light'
+    if(count%2 == 0){
+        className = 'list-group-item-light'
+        count = count + 1 
+    } else {
+        className = 'list-group-item-dark'
+        count = count + 1 
+    }
+
+    let liTask = $('<li>',{
+        'class' : 'list-group-item '+className,
+        text    : inputNewTask.val()
+    })
+
+    liTask.click(() => {
+        liTask.toggleClass("done"); 
+    })
+
+    ulTasks.append(liTask)
+    inputNewTask.val('')
+}
+
+
+btnAddTask.click(addItem)
+
+btnClearTask.click(() => {
+    inputNewTask.val('')
+})
+
+inputNewTask.keypress((e) => {
+    if(e.which == 13) addItem()
+})
